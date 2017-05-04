aaadf5ff844baab8fa3e9a3a1fead0b144cb51e0
Nathan Toombs <nathan.toombs@classyllama.com>
2017-05-04 09:41:23 -0500

    Add code QA fixes

--- registration.php
+++ registration.php
@@ -1,8 +1,18 @@
 <?php
 /**
- * @category    ClassyLlama
- * @copyright   Copyright (c) 2017 Classy Llama
+ * ClassyLlama_Vimeo
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @copyright  Copyright (c) 2017 Classy Llama.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
+
 use \Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(ComponentRegistrar::MODULE, 'MagentoEse_Vimeo', __DIR__);
--- view/frontend/web/js/load-player.js
+++ view/frontend/web/js/load-player.js
@@ -1,11 +1,20 @@
 /**
- * @category    ClassyLlama
- * @copyright   Copyright (c) 2017 Classy Llama
+ * ClassyLlama_Vimeo
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @copyright  Copyright (c) 2017 Classy Llama.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
 
 define([
     'jquery',
-    'MagentoEse_Vimeo/js/player.min',
+    'MagentoEse_Vimeo/js/player.min', // Vimeo API
     'jquery/ui',
     'Magento_ProductVideo/js/load-player'
 ], function ($, Player) {
